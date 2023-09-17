3feefd7b4eb708ab58a9cf373a2e173baef5df98
shengyi <13720352398@163.com>
2023-09-17 21:24:47 +0800

    xpath

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/sy973961684/crawler
 go 1.20
 
 require (
+	github.com/antchfx/htmlquery v1.3.0
 	golang.org/x/net v0.15.0
 	golang.org/x/text v0.13.0
+)
+
+require (
+	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/antchfx/htmlquery"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 	"io"
 	"net/http"
-	"regexp"
 )
-
-var headerRe = regexp.MustCompile(`[\s\S]*?img alt="([\s\S]*?)"[\s\S]*?`)
 
 func main() {
 	url := "https://www.thepaper.cn"
@@ -23,11 +22,11 @@ func main() {
 		return
 	}
 
-	mathers := headerRe.FindAllSubmatch(body, -1)
-	for _, m := range mathers {
-		fmt.Println("fetch card news:", string(m[1]))
+	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	nodes := htmlquery.Find(doc, `/html/body/div/main/div[2]/div[3]/div[2]/div[1]/div[3]/div[1]/div/div[2]/div/div[2]/div/div/div/div[1]/a/h2`)
+	for _, node := range nodes {
+		fmt.Println("fetch card ", node.FirstChild.Data)
 	}
-
 }
 
 func Fetch(url string) ([]byte, error) {
