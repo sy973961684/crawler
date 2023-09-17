ef6c684554a809ba200909f6c68f8171bdea2cc2
shengyi <13720352398@163.com>
2023-09-17 13:29:20 +0800

    encoding xxx

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/sy973961684/crawler
 
 go 1.20
+
+require (
+	golang.org/x/net v0.15.0
+	golang.org/x/text v0.13.0
+)
--- main.go
+++ main.go
@@ -1,49 +1,54 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io"
 	"net/http"
-	"strings"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Printf("fetch url error:%v\n", err)
 		return
 	}
 
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Printf("Error close io:%v", err)
-		}
-	}(resp.Body)
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
+		fmt.Printf("Error status code:%d", resp.StatusCode)
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeteminEncoding(bodyReader)
+	uft8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(uft8Reader)
+}
+
+func DeteminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1023)
 
 	if err != nil {
-		fmt.Printf("read content failed:%v", err)
-		return
+		fmt.Printf("fetch error:%v", err)
+		return unicode.UTF8
 	}
 
-	numLinks := strings.Count(string(body), "<a")
-	fmt.Printf("homepage has %d links!\n", numLinks)
-
-	numLinks = bytes.Count(body, []byte("<a"))
-	fmt.Printf("homepage has %d links!\n", numLinks)
-
-	exist := strings.Contains(string(body), "疫情")
-	fmt.Printf("是否存在疫情:%v\n", exist)
-
-	exist = bytes.Contains(body, []byte("疫情"))
-	fmt.Printf("是否存在疫情:%v\n", exist)
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
